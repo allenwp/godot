a36fa01bd1942e7272bbe276b7c1623ad75cda6f
Allen Pestaluky <allenpestaluky@gmail.com>
2025-05-29 12:19:00 -0400

    Add `white` and `contrast` parameters to the AgX tonemapper.
    Also optimize all tonemappers to perform less calculations per-pixel.

--- drivers/gles3/shaders/effects/post.glsl
+++ drivers/gles3/shaders/effects/post.glsl
@@ -140,7 +140,7 @@ void main() {
 #endif // USE_GLOW
 
 	color.rgb = srgb_to_linear(color.rgb);
-	color.rgb = apply_tonemapping(color.rgb, white);
+	color.rgb = apply_tonemapping(color.rgb);
 	color.rgb = linear_to_srgb(color.rgb);
 
 #ifdef USE_BCS
